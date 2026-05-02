51fa48487abb69f0daec54c99b88a6550ad9f3b2
Fat Han Nuraddin <fat.han.nuraddin@gmail.com>
2026-05-02 16:41:08 +0700

    Init workflow

---
+++ build.py
@@ -0,0 +1,42 @@
+import shutil
+from pathlib import Path
+import gspread
+import os
+
+# Vars
+GSHEET_ID = os.environ['GSHEET_ID']
+GSHEET_TAB = os.environ['GSHEET_TAB']
+
+# Prepare output folder
+dist = Path("dist")
+shutil.rmtree(dist, ignore_errors=True)
+dist.mkdir(parents=True, exist_ok=True)
+
+
+# Load credentials and authorize the client
+gc = gspread.service_account(filename='service_account.json')
+
+# Open the sheet by ID
+sh = gc.open_by_key(GSHEET_ID)
+
+# Select the worksheet by its tab name
+worksheet = sh.worksheet(GSHEET_TAB)
+
+# Get all records as a list of dictionaries
+data = worksheet.get_all_records()
+
+# Write files
+for row in data:
+    path = dist / row['path']
+    content = row.get('content', '')
+
+    # Create the folder if it doesn't exist
+    folder = os.path.dirname(path)
+    if folder:
+        os.makedirs(folder, exist_ok=True)
+
+    # Create and write the file
+    with open(path, "w") as file:
+        file.write(content)
+        print(f"Successfully created: {path}")
+
